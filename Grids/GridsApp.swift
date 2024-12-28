2a8b2e19cb30c4880cd8726eaa04324e09808b5d
Marquis <software@marquiskurt.net>
2024-12-28 14:23:20 -0500

    Initial Commit

---
+++ Grids/GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridsApp.swift
+//  Grids
+//
+//  Created by Marquis Kurt on 28-12-2024.
+//
+
+import SwiftUI
+
+@main
+struct GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GridsTests/GridsTests.swift
@@ -0,0 +1,16 @@
+//
+//  GridsTests.swift
+//  GridsTests
+//
+//  Created by Marquis Kurt on 28-12-2024.
+//
+
+import Testing
+
+struct GridsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
