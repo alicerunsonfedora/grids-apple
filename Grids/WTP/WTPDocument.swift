69f0503f9180049f276740341bb9b7a842ca7c1c
Marquis <software@marquiskurt.net>
2024-12-28 19:38:07 -0500

    Add remaining icon cases

--- Grids/WTP/WTPDocument.swift
+++ Grids/WTP/WTPDocument.swift
@@ -11,6 +11,12 @@ struct WTPDocument {
     enum FileIcon: Int, Identifiable {
         case generic = 1
         case gardens = 2
+        case mill = 3
+        case mines = 4
+        case shrine = 5
+        case challenge = 7
+        case set = 9
+        case tutorial = 10
 
         var name: String {
             switch self {
@@ -18,6 +24,18 @@ struct WTPDocument {
                 "Generic"
             case .gardens:
                 "Gardens"
+            case .mill:
+                "Mill"
+            case .mines:
+                "Mines"
+            case .shrine:
+                "Shrine"
+            case .challenge:
+                "Challenge"
+            case .set:
+                "Puzzle Set"
+            case .tutorial:
+                "Tutorial"
             }
         }
 
